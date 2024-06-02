46daa30aad0b526f9df14270aee4f6480fa29ca6
Stefan Rosemann <info@stefan-rosemann.de>
2024-06-02 22:55:50 +0200

    ERC721

--- test/FoundryNFT.t.sol
+++ test/FoundryNFT.t.sol
@@ -5,16 +5,16 @@ import {Test, console} from "forge-std/Test.sol";
 import {FoundryNFT} from "../src/FoundryNFT.sol";
 
 contract FoundryNFTTest is Test {
-    //FoundryNFT public foundryNFT;
-
     function testFoundryNFT() public {
         FoundryNFT foundryNFT = new FoundryNFT();
         assertEq(foundryNFT.name(), "FoundryNFT");
     }
-    
+
     function testMint() public {
         FoundryNFT foundryNFT = new FoundryNFT();
+        uint256 ts = foundryNFT.totalSupply();
+
         foundryNFT.mint();
-        assertEq(foundryNFT.totalSupply(), 1);
+        assertEq(foundryNFT.totalSupply(), ts + 1);
     }
 }
